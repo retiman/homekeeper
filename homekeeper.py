f6972b6c300ef9ae6cbd605b6680ea440bed1437
Min Huang <min.huang@alumni.usc.edu>
2013-11-18 18:03:55 -0800

    Initial commit

---
+++ homekeeper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+import optparse
+import os
+import shutil
+
+parser = optparse.OptionParser()
+parser.add_option('', '-h', '--help', help='Display this message.')
+(opts, args) = parser.parse_args()
