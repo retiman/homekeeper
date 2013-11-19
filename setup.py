2230c322a0966f765b7dc9fcfc5cf4cb37e33f90
Min Huang <min.huang@alumni.usc.edu>
2013-11-18 19:32:54 -0800

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+
+from setuptools import setup
+
+setup(
+    author='Min Huang',
+    author_email='min.huang@alumni.usc.edu',
+    description='Homekeeper can version your dotfiles with Git.',
+    long_description=open('README.txt').read(),
+    name='homekeeper',
+    package_dir={'': 'lib'},
+    scripts=['bin/homekeeper'],
+    test_suite='homekeeper_test',
+    version='2.0',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Topic :: Utilities',
+    ],
+)
