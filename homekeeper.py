9e6cc8656bda329879b4d5e74f9e2927dd20e75b
Min Huang <min.huang@alumni.usc.edu>
2013-11-18 18:36:41 -0800

    Secondary commit

--- homekeeper.py
+++ homekeeper.py
@@ -2,7 +2,27 @@
 import optparse
 import os
 import shutil
+import subprocess
+import sys
 
-parser = optparse.OptionParser()
-parser.add_option('', '-h', '--help', help='Display this message.')
-(opts, args) = parser.parse_args()
+def print_usage():
+    print 'Usage: homekeeper [command]'
+    print 'Commands:'
+    print '  link              symlink dotfiles and remove broken symlinks.'
+    print '  save              save the last commit in master branch.'
+    print '  track             track the dotfile with homekeeper.'
+    print '  update            pull from master and merge into this branch.'
+
+def sh(command_array):
+    print ' '.join(command_array)
+    p = subprocess.Popen(command_array, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
+def branch():
+    return sh(['git', 'status']).split('\n')[0].split('# On branch ')[1]
+
+def commit():
+    return sh(['git', 'show', 'HEAD']).split('\n')[0].split(' ')[1]
+
