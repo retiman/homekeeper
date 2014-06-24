e1f11aef6fa3782d57f99e92a48904c22d22f040
Min Huang <min.huang@alumni.usc.edu>
2014-06-23 18:31:29 -0700

    Put this in the global rc

--- homekeeper/__init__.py
+++ homekeeper/__init__.py
@@ -9,7 +9,6 @@ import sys
 VERSION = '2.3.0'
 
 
-# pylint: disable=too-few-public-methods
 class _cd(object):
     "Use with the `with` keyword to change directory."""
     def __init__(self, pathname):
