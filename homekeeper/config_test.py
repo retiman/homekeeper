53c15821712ebadb3e1d0a7e719125caee00f4a4
Min Huang <min.huang@alumni.usc.edu>
2014-10-11 11:50:10 -0700

    Write a default configuration for testing

--- homekeeper/config_test.py
+++ homekeeper/config_test.py
@@ -1,10 +1,11 @@
 import fake_filesystem
 import homekeeper.config
+import json
 import unittest
 
 # pylint: disable=invalid-name
 os = None
-config = homekeeper.config
+Config = homekeeper.config.Config
 
 class ConfigTest(unittest.TestCase):
     def setUp(self):
@@ -14,17 +15,33 @@ class ConfigTest(unittest.TestCase):
         self.filesystem = fake_filesystem.FakeFilesystem()
         os = fake_filesystem.FakeOsModule(self.filesystem)
         homekeeper.config.os = os
+        self.write_configuration()
 
     def tearDown(self):
         del self.filesystem
 
+    def write_configuration(self):
+        config = {
+            'base': os.path.join(os.getenv('HOME'), 'dotfiles-base'),
+            'directory': os.path.join(os.getenv('HOME'), 'dotfiles-main'),
+            'excludes': ['.git'],
+            'override': True
+        }
+        pathname = Config.PATHNAME
+        self.filesystem.CreateFile(pathname)
+        config_file = open(pathname, 'w')
+        config_file.write(json.dumps(config))
+        config_file.close()
+
     def test_defaults_no_configuration_file(self):
-        self.config = config.Config()
+        os.unlink(Config.PATHNAME)
+        self.config = Config()
+        self.assertFalse(os.path.exists(Config.PATHNAME))
         self.assertEquals(self.config.base,
-                          config.Config.DEFAULTS['base'])
+                          Config.DEFAULTS['base'])
         self.assertEquals(self.config.directory,
-                          config.Config.DEFAULTS['directory'])
+                          Config.DEFAULTS['directory'])
         self.assertEquals(self.config.excludes,
-                          config.Config.DEFAULTS['excludes'])
+                          Config.DEFAULTS['excludes'])
         self.assertEquals(self.config.override,
-                          config.Config.DEFAULTS['override'])
+                          Config.DEFAULTS['override'])
