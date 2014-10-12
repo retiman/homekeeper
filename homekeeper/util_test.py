ce921502166304195ff23716b0e4e98daecf0307
Min Huang <min.huang@alumni.usc.edu>
2014-10-12 01:09:22 -0700

    Test create_symlinks here

--- homekeeper/util_test.py
+++ homekeeper/util_test.py
@@ -5,17 +5,30 @@ import __builtin__
 
 # pylint: disable=invalid-name
 os = None
-util = homekeeper.util
 
 class UtilTest(unittest.TestCase):
     def setUp(self):
         self.filesystem = fake_filesystem.FakeFilesystem()
         globals()['os'] = fake_filesystem.FakeOsModule(self.filesystem)
         __builtin__.open = fake_filesystem.FakeFileOpen(self.filesystem)
+        self.home = '/home/johndoe'
         homekeeper.util.os = os
+        homekeeper.util.os.getenv = lambda var: self.home
 
     def tearDown(self):
         del self.filesystem
+
+    def test_create_symlinks(self):
+        source = '/dotfiles/.vimrc'
+        target = self.home + '/.vimrc'
+        self.filesystem.CreateFile(source)
+        self.assertTrue(os.path.exists(source))
+        self.assertFalse(os.path.exists(target))
+        homekeeper.util.create_symlinks('/dotfiles', self.home)
+        self.assertTrue(os.path.exists(source))
+        self.assertTrue(os.path.exists(target))
+        self.assertTrue(os.path.islink(target))
+        self.assertEquals(source, os.readlink(target))
 
     def test_cleanup_symlinks(self):
         self.filesystem.CreateFile('/a.txt')
