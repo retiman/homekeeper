399ca7bc1d7b0d1eaed3f1d70f9ff5e86721f3eb
Min Huang <min.huang@alumni.usc.edu>
2015-03-01 22:57:22 -0800

    Extract builtin replacement into a function

--- homekeeper/testing.py
+++ homekeeper/testing.py
@@ -11,12 +11,16 @@ def create_fake_filesystem():
         The created fake filesystem object and os module.
     """
     filesystem = fake_filesystem.FakeFilesystem()
-    __builtin__.open = fake_filesystem.FakeFileOpen(filesystem)
     # pylint: disable=invalid-name
     os = fake_filesystem.FakeOsModule(filesystem)
     os.getenv = _getenv
     _replace_modules(os)
+    _replace_builtins(filesystem)
     return [filesystem, os]
+
+def _replace_builtins(filesystem):
+    """Replaces Python builtins."""
+    __builtin__.open = fake_filesystem.FakeFileOpen(filesystem)
 
 def _replace_modules(fake_os):
     """Replaces filesystem modules and functions with fakes."""
