be96ea4900f11b1f8c8bba7592d3c19733776043
Min Huang <min.huang@alumni.usc.edu>
2015-03-03 14:59:39 -0800

    More appropriate to mock here

--- homekeeper/test.py
+++ homekeeper/test.py
@@ -1,11 +1,13 @@
 import homekeeper
 import homekeeper.config
 import homekeeper.testing
+import mock
 import unittest
 
 # pylint: disable=invalid-name
 config = homekeeper.config
 os = None
+patch = mock.patch
 testing = homekeeper.testing
 
 class HomekeeperTest(unittest.TestCase):
@@ -44,41 +46,35 @@ class HomekeeperTest(unittest.TestCase):
         self.assertEquals(os.path.join(self.config.directory, '.vimrc'),
                           os.readlink(os.path.join(self.home, '.vimrc')))
 
-    def test_link_with_excludes(self):
+    @patch('homekeeper.util.create_symlinks')
+    @patch('homekeeper.util.cleanup_symlinks')
+    def test_link_with_excludes(self,
+                                mock_cleanup_symlinks,
+                                mock_create_symlinks):
         self.config.excludes = ['.bashrc']
         self.config.save(testing.configuration_file())
         self.homekeeper = homekeeper.Homekeeper()
         self.filesystem.CreateFile(os.path.join(self.config.base, '.bashrc'))
         self.filesystem.CreateFile(os.path.join(self.home, '.bashrc'))
         self.homekeeper.link()
-        self.assertFalse(os.path.islink(os.path.join(self.home, '.bashrc')),
-                         msg='The excluded file was symlinked anyways.')
+        mock_create_symlinks.assert_called_with(self.config.directory,
+                                                self.home,
+                                                excludes=self.config.excludes,
+                                                includes=self.config.includes)
+        mock_cleanup_symlinks.assert_called_with(os.getenv('HOME'))
 
-    def test_link_with_includes(self):
+    @patch('homekeeper.util.create_symlinks')
+    @patch('homekeeper.util.cleanup_symlinks')
+    def test_link_with_includes(self,
+                                mock_cleanup_symlinks,
+                                mock_create_symlinks):
         self.config.includes = [os.path.join('.foo', 'bar', 'baz')]
         self.config.save(testing.configuration_file())
         self.homekeeper = homekeeper.Homekeeper()
-        self.filesystem.CreateFile(os.path.join(self.config.base,
-                                                '.foo',
-                                                'bar',
-                                                'baz'))
-        self.filesystem.CreateFile(os.path.join(self.home,
-                                                '.foo',
-                                                'bar',
-                                                'unrelated'))
         self.homekeeper.link()
-        self.assertFalse(os.path.islink(os.path.join(self.home, '.foo')),
-                         msg='The directory was symlinked anyways.')
-        self.assertTrue(os.path.islink(os.path.join(self.home,
-                                                    '.foo',
-                                                    'bar',
-                                                    'baz')))
-        self.assertTrue(os.path.exists(os.path.join(self.home,
-                                                    '.foo',
-                                                    'bar',
-                                                    'unrelated')))
-        self.assertFalse(os.path.islink(os.path.join(self.home,
-                                                     '.foo',
-                                                     'bar',
-                                                     'unrelated')))
+        mock_create_symlinks.assert_called_with(self.config.directory,
+                                                self.home,
+                                                excludes=self.config.excludes,
+                                                includes=self.config.includes)
+        mock_cleanup_symlinks.assert_called_with(os.getenv('HOME'))
 
