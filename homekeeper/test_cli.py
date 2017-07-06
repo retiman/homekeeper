66e29bca3ae1bf4e062a4fbc955d7fbb84a4b887
Min Huang <min.huang@alumni.usc.edu>
2017-07-06 16:47:25 -0700

    Renames to just run

--- homekeeper/test_cli.py
+++ homekeeper/test_cli.py
@@ -10,40 +10,40 @@ class TestCli(object):
     def setup_method(self):
         self.runner = click.testing.CliRunner()
 
-    def verify_run_success(self, *args):
+    def run(self, *args):
         result = self.runner.invoke(homekeeper.cli.main, args)
         assert result.output == ''
         assert result.exit_code == 0
 
     def test_cleanup(self, MockHomekeeper):
-        self.verify_run_success('cleanup')
-        MockHomekeeper.assert_called_once_with(cleanup_symlinks=True)
+        self.run('cleanup')
         MockHomekeeper.cleanup.assert_called_once()
+        MockHomekeeper.assert_called_once_with(cleanup_symlinks=True)
 
     def test_clean_ignores_no_cleanup(self, MockHomekeeper):
-        self.verify_run_success('--no-cleanup', 'cleanup')
+        self.run('--no-cleanup', 'cleanup')
         MockHomekeeper.assert_called_once_with(cleanup_symlinks=True)
         MockHomekeeper.cleanup.assert_called_once()
 
     def test_init_with_custom_config(self, MockHomekeeper):
         dotfiles_directory = os.getcwd()
-        config_path = os.path.join(dotfiles_directory,'.config.json')
-        self.verify_run_success('--config-path', config_path, 'init')
+        config_path = os.path.join(dotfiles_directory, '.config.json')
+        self.run('--config-path', config_path, 'init')
+        MockHomekeeper.init.assert_called_once()
         MockHomekeeper.assert_called_once_with(config_path=config_path,
                                                cleanup_symlinks=False,
                                                overwrite=False)
-        MockHomekeeper.init.assert_called_once()
 
     def test_init_cannot_cleanup_or_overwrite(self, MockHomekeeper):
-        self.verify_run_success('--cleanup', '--overwrite', 'init')
+        self.run('--cleanup', '--overwrite', 'init')
+        MockHomekeeper.init.assert_called_once()
         MockHomekeeper.assert_called_once_with(config_path=None,
                                                cleanup_symlinks=False,
                                                overwrite=False)
-        MockHomekeeper.init.assert_called_once()
 
     def test_keep_with_default_options(self, MockHomekeeper):
-        self.verify_run_success('keep')
+        self.run('keep')
+        MockHomekeeper.keep.assert_called_once()
         MockHomekeeper.assert_called_once_with(config_path=None,
                                                cleanup_symlinks=True,
                                                overwrite=True)
-        MockHomekeeper.keep.assert_called_once()
