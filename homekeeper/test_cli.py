75894f888750c50396e2e364dc3bd7e8e3b4c5cf
Min Huang <min.huang@alumni.usc.edu>
2017-07-06 16:40:39 -0700

    Fixes tests to actually assert on the right thing

--- homekeeper/test_cli.py
+++ homekeeper/test_cli.py
@@ -10,37 +10,34 @@ class TestCli(object):
     def setup_method(self):
         self.runner = click.testing.CliRunner()
 
-    def test_clean(self, mock):
-        args = ['clean']
+    def verify_run_success(self, *args):
         result = self.runner.invoke(homekeeper.cli.main, args)
         assert result.output == ''
         assert result.exit_code == 0
-        assert mock.called_once_with(args[0])
-        assert mock.clean.called_once()
 
-    def test_init_with_custom_config(self, mock):
+    def test_clean(self, MockHomekeeper):
+        self.verify_run_success('clean')
+        MockHomekeeper.assert_called_once_with(cleanup_symlinks=True)
+        MockHomekeeper.cleanup.assert_called_once()
+
+    def test_init_with_custom_config(self, MockHomekeeper):
         dotfiles_directory = os.getcwd()
-        custom_homekeeper_json = os.path.join(dotfiles_directory,
-                                              '.config.json')
-        args = ['--config-path', custom_homekeeper_json, 'init']
-        result = self.runner.invoke(homekeeper.cli.main, args)
-        assert result.output == ''
-        assert result.exit_code == 0
-        assert mock.called_once_with(config_path=custom_homekeeper_json,
-                                     cleanup_symlinks=False, overwrite=False)
-        assert mock.init.called_once()
+        config_path = os.path.join(dotfiles_directory,'.config.json')
+        self.verify_run_success('--config-path', config_path, 'init')
+        MockHomekeeper.assert_called_once_with(config_path=config_path,
+                                               cleanup_symlinks=False,
+                                               overwrite=False)
+        MockHomekeeper.init.assert_called_once()
 
-    def test_init_cannot_cleanup_or_overwrite(self, mock):
-        args = ['--cleanup', '--overwrite', 'init']
-        result = self.runner.invoke(homekeeper.cli.main, args)
-        assert result.output == ''
-        assert result.exit_code == 0
-        assert mock.called_once_with(config_path=None, cleanup_symlinks=True,
-                                     overwrite=False)
-        assert mock.init.called_once()
+    def test_init_cannot_cleanup_or_overwrite(self, MockHomekeeper):
+        self.verify_run_success('--cleanup', '--overwrite', 'init')
+        MockHomekeeper.assert_called_once_with(config_path=None,
+                                               cleanup_symlinks=False,
+                                               overwrite=False)
+        MockHomekeeper.init.assert_called_once()
 
-    #def test_keep_with_default_option(self, mock):
-    #    args = ['keep']
-    #    self.runner.invoke(homekeeper.cli.main, args)
-    #    assert mock.called_once_with()
-    #    assert mock.init.called_once()
+    # def test_keep_with_default_option(self, mock):
+    #     args = ['keep']
+    #     self.runner.invoke(homekeeper.cli.main, args)
+    #     assert mock.called_once_with()
+    #     assert mock.init.called_once()
