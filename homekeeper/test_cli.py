fb148552c164511ee9fb92f6d3e4800e0648bde7
Min Huang <min.huang@alumni.usc.edu>
2017-07-06 22:57:21 -0700

    Adds tests for keep/unkeep

--- homekeeper/test_cli.py
+++ homekeeper/test_cli.py
@@ -40,21 +40,42 @@ class TestCli(homekeeper.test_case.TestCase):
         dotfiles_directory = os.getcwd()
         config_path = os.path.join(dotfiles_directory, '.config.json')
         self.run('--config-path', config_path, 'init')
-        self.mock_class.init.assert_called_once()
         self.mock_class.assert_called_once_with(config_path=config_path)
+        self.mock_class.init.assert_called_once()
         assert self.mock_instance.cleanup_symlinks == False
         assert self.mock_instance.overwrite == False
 
     def test_init_cannot_cleanup_or_overwrite(self):
         self.run('--cleanup-symlinks', '--overwrite', 'init')
-        self.mock_class.init.assert_called_once()
         self.mock_class.assert_called_once_with(config_path=None)
+        self.mock_class.init.assert_called_once()
         assert self.mock_instance.cleanup_symlinks == False
         assert self.mock_instance.overwrite == False
 
     def test_keep_with_default_options(self):
         self.run('keep')
-        self.mock_class.keep.assert_called_once()
         self.mock_class.assert_called_once_with(config_path=None)
+        self.mock_class.keep.assert_called_once()
         assert self.mock_instance.cleanup_symlinks == True
         assert self.mock_instance.overwrite == True
+
+    def test_keep_with_custom_options(self):
+        self.run('--no-cleanup-symlinks', '--no-overwrite', 'keep')
+        self.mock_class.assert_called_once_with(config_path=None)
+        self.mock_class.keep.assert_called_once()
+        assert self.mock_instance.cleanup_symlinks == False
+        assert self.mock_instance.overwrite == False
+
+    def test_unkeep_with_default_options(self):
+        self.run('unkeep')
+        self.mock_class.assert_called_once_with(config_path=None)
+        self.mock_class.keep.assert_called_once()
+        assert self.mock_instance.cleanup_symlinks == True
+        assert self.mock_instance.overwrite == True
+
+    def test_unkeep_with_custom_options(self):
+        self.run('--no-cleanup-symlinks', '--no-overwrite', 'unkeep')
+        self.mock_class.assert_called_once_with(config_path=None)
+        self.mock_class.keep.assert_called_once()
+        assert self.mock_instance.cleanup_symlinks == False
+        assert self.mock_instance.overwrite == False
