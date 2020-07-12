28154b398a1e2d88fa13f2152d7824fbc9825e35
Min Huang <min.huang@alumni.usc.edu>
2020-07-11 22:25:05 -0700

    Updates with a license and url

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ import setuptools
 
 
 setuptools.setup(
+    name='homekeeper',
+    version=homekeeper.__version__,
+    license='MIT',
     author='Min Huang',
     author_email='min.huang@alumni.usc.edu',
+    url='https://github.com/retiman/homekeeper',
     description='Symlinks your dotfiles from anywhere to your home directory.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    name='homekeeper',
     packages=['homekeeper'],
     install_requires=['click >= 7.1.2'],
-    version=homekeeper.__version__,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Utilities',
