27978ed649095d1af689241c022b4291d95ef5bf
Min Huang <min.huang@alumni.usc.edu>
2020-07-11 22:36:41 -0700

    Fixes markdown errors by upgrading setuptools

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ import homekeeper
 import setuptools
 
 
+with open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     author='Min Huang',
     author_email='min.huang@alumni.usc.edu',
     description='Symlinks your dotfiles from anywhere to your home directory.',
     install_requires=['click >= 7.1.2'],
     license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     name='homekeeper',
     packages=['homekeeper'],
     platforms=['any'],
