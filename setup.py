649610f905eedd465c223dcd468fef63974eb5fb
Min Huang <min.huang@alumni.usc.edu>
2021-04-14 20:07:26 -0700

    Updates supported versions

--- setup.py
+++ setup.py
@@ -21,10 +21,8 @@ setuptools.setup(
     version=homekeeper.__version__,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Utilities',
     ],
     entry_points="""
