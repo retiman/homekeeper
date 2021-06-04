7a22729d60d9225c3907aed31da5f1fc6fa90163
Min Huang <min.huang@alumni.usc.edu>
2021-06-03 20:52:56 -0700

    Sets values

--- pkg/common/app_test.go
+++ pkg/common/app_test.go
@@ -1,11 +1,21 @@
 package common
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
+)
+
+var (
+	// Even though Windows does support symlinks, by default, they are only available if the system administrator enables
+	// them.  The rationale is that most applications are not written to support them, and enabling them by default would
+	// present a security risk.
+	IsSymlinkSupported bool
+	IsReadlinkSupported bool
+	IsLstatSupported bool
 )
 
 type Paths struct {
@@ -22,7 +32,7 @@ func init() {
 		DisableQuote:     true,
 	})
 
-	log.SetLevel(log.TraceLevel)
+	log.SetLevel(log.DebugLevel)
 }
 
 func GetRepositoryRoot() (repositoryRoot string, err error) {
@@ -37,11 +47,11 @@ func GetRepositoryRoot() (repositoryRoot string, err error) {
 
 		_, err = os.Stat(filepath.Join(repositoryRoot, ".git"))
 		if err == nil {
-			log.Debugf("found repository root: %s", repositoryRoot)
+			log.Tracef("found repository root: %s", repositoryRoot)
 			return
 		}
 
-		if os.IsNotExist(err) {
+		if errors.Is(err, os.ErrNotExist) {
 			continue
 		}
 
@@ -71,39 +81,91 @@ func SetupFiles() (paths *Paths, err error) {
 	// you didn't intend to, but the GetRepositoryRoot() method should prevent you from doing that).
 	//
 	// This way we can test out different platforms as well.
-	log.Debugf("removing all files in directory: %s", paths.RootDirectory)
+	log.Tracef("removing all files in directory: %s", paths.RootDirectory)
 	err = os.RemoveAll(paths.RootDirectory)
 	if err != nil {
 		return
 	}
 
-	directories := []string{
+	_, err = CreateTestDirectories(paths)
+	if err != nil {
+		return
+	}
+
+	_, err = CreateTestFiles(paths)
+	if err != nil {
+		return
+	}
+
+	_, err = CreateTestSymlinks(paths)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func CreateTestDirectories(paths *Paths) (directories []string, err error) {
+	directories = []string{
 		paths.DotfilesDirectory,
 		filepath.Join(paths.DotfilesDirectory, ".git"),
 		filepath.Join(paths.DotfilesDirectory, ".vim"),
 	}
 
 	for _, directory := range directories {
-		log.Debugf("creating test directory: %s", directory)
+		log.Tracef("creating test directory: %s", directory)
 		err = os.MkdirAll(directory, os.ModePerm)
 		if err != nil {
 			return
 		}
 	}
 
-	files := []string{
+	return
+}
+
+func CreateTestFiles(paths *Paths) (files []string, err error) {
+	files = []string{
 		filepath.Join(paths.DotfilesDirectory, ".bash_profile"),
 		filepath.Join(paths.DotfilesDirectory, ".gitconfig"),
 		filepath.Join(paths.DotfilesDirectory, ".vimrc"),
 	}
 
 	for _, file := range files {
-		log.Debugf("creating test file: %s", file)
+		log.Tracef("creating test file: %s", file)
 		_, err = os.Create(file)
 		if err != nil {
 			return
 		}
 	}
 
+	return
+}
+
+func CreateTestSymlinks(paths *Paths) (symlinks []string, err error) {
+	source := filepath.Join(paths.DotfilesDirectory, ".bash_profile")
+	target := filepath.Join(paths.DotfilesDirectory, ".bashrc")
+	symlinks = []string{target}
+
+	log.Tracef("symlinking %s -> %s", source, target)
+	err = os.Symlink(source, target)
+	if err != nil {
+		IsSymlinkSupported = false
+		log.Warnf("symlink is not supported on this system: %+v", err)
+		return
+	}
+
+	_, err = os.Readlink(target)
+	if err != nil {
+		IsReadlinkSupported = false
+		log.Warnf("readlink is not supported on this system: %+v", err)
+	}
+
+	_, err = os.Lstat(target)
+	if err != nil {
+		IsLstatSupported = false
+		log.Warnf("lstat is not supported on this system: %+v", err)
+	}
+
+	err = nil
 	return
 }
